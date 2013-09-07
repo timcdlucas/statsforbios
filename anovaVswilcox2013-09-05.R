8656fe32e76fe79d2274789f73a1ccbf35582a79
tim lucas <timcdlucas@gmail.com>
2013-09-07 13:58:10 +0100

    final sim added to wilvoxvsanova

--- anovaVswilcox2013-09-05.R
+++ anovaVswilcox2013-09-05.R
@@ -43,7 +43,7 @@ dev.off()
 
 
 
-# To compare anova and wilcox test, I'll draw numbers from normal distributions and apply the test. Do this for different sample sizes and replicate.
+# To compare anova and wilcox test, I'll draw numbers from normal distributions and apply the test. Do this for different standard deviations and replicate.
 # What sample sizes and how many replicates of each.
 sdvec <- c(seq(0.02,0.98, by=0.02), seq(1,5,by=0.2))
 reps <- 500
@@ -61,6 +61,44 @@ sd <- sdvec[i]
 # Replicate tests with different random numbers.
 for(r in 1:reps){
 d <- cbind(c(rnorm(30,mean=1,sd=sd),rnorm(30,mean=1.5,sd=sd)), c(rep(1,30),rep(2,30)))
+
+pAnova[i,r] <- (anova(lm(d[,1]~d[,2])))$Pr[1]
+pWilcox[i,r] <- (wilcox.test(d[,1]~d[,2]))$p.value
+
+}
+}
+
+#plot
+	
+png('anovaVsWilcox-sd.png')
+par(cex=1.3)
+plot(apply(pAnova,1,mean)~sdvec,type='l', col='Blue', xlab='Standard Deviation', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and n=30')
+lines(apply(pWilcox,1,mean)~sdvec,type='l', col='Red', lwd=3)
+legend('bottomright', inset=0.01,fill=c('Red','Blue'),legend=c('Anova', 'Wilcox'), box.lwd=2)
+box(lwd=2)
+dev.off()
+
+
+
+
+
+
+# To compare anova and wilcox test, I'll draw numbers from normal distributions and apply the test. Do this for different effect sizes and replicate.
+# What sample sizes and how many replicates of each.
+effectvec <- seq(0.1,5,by=0.1)
+reps <- 50
+
+# Empty matrices to fill
+pAnova <- matrix(0,nrow=length(effectvec),ncol=reps)
+pWilcox <- matrix(0,nrow=length(effectvec),ncol=reps)
+
+
+# Run tests for each sample size (this loop is slow. I should rewrite it.)
+for(i in 1:length(effectvec)){
+
+# Replicate tests with different random numbers.
+for(r in 1:reps){
+d <- cbind(c(rnorm(30,mean=1,sd=1),rnorm(30,mean=1+effectvec[i],sd=1)), c(rep(1,30),rep(2,30)))
 
 pAnova[i,r] <- (anova(lm(d[,1]~d[,2])))$Pr[1]
 pWilcox[i,r] <- (wilcox.test(d[,1]~d[,2]))$p.value
