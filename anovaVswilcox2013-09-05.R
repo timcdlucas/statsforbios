d794a5ab285a3cff59c3b04bd13ae17dff452fa3
tim lucas <timcdlucas@gmail.com>
2013-09-07 12:54:13 +0100

    Next wilcoxvsanova simulation

--- anovaVswilcox2013-09-05.R
+++ anovaVswilcox2013-09-05.R
@@ -43,3 +43,41 @@ dev.off()
 
 
 
+# To compare anova and wilcox test, I'll draw numbers from normal distributions and apply the test. Do this for different sample sizes and replicate.
+# What sample sizes and how many replicates of each.
+sdvec <- seq(0.2,10, by=0.4)
+reps <- 500
+
+# Empty matrices to fill
+pAnova <- matrix(0,nrow=length(sdvec),ncol=reps)
+pWilcox <- matrix(0,nrow=length(sdvec),ncol=reps)
+
+
+# Run tests for each sample size (this loop is slow. I should rewrite it.)
+for(i in 1:length(sdvec)){
+
+sd <- sdvec[i]
+
+# Replicate tests with different random numbers.
+for(r in 1:reps){
+d <- cbind(c(rnorm(30,mean=1,sd=sd),rnorm(30,mean=1.5,sd=sd)), c(rep(1,30),rep(2,30)))
+
+pAnova[i,r] <- (anova(lm(d[,1]~d[,2])))$Pr[1]
+pWilcox[i,r] <- (wilcox.test(d[,1]~d[,2]))$p.value
+
+}
+}
+
+#plot
+
+png('anovaVsWilcox-sd.png')
+par(cex=1.3)
+plot(apply(pAnova,1,mean)~sdvec,type='l', col='Blue', xlab='Standard Deviation', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and n=30')
+lines(apply(pWilcox,1,mean)~sdvec,type='l', col='Red', lwd=3)
+legend('topright', inset=0.01,fill=c('Red','Blue'),legend=c('Anova', 'Wilcox'), box.lwd=2)
+box(lwd=2)
+dev.off()
+
+
+
+
