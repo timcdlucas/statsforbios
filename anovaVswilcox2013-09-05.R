a0aa9a9dfd020c7bee367500df2444442a27b174
tim lucas <timcdlucas@gmail.com>
2013-09-08 13:16:29 +0100

    Quantiles added

--- anovaVswilcox2013-09-05.R
+++ anovaVswilcox2013-09-05.R
@@ -5,8 +5,8 @@ setwd('~/Dropbox/Documents/statsforbios')
 
 # To compare anova and wilcox test, I'll draw numbers from normal distributions and apply the test. Do this for different sample sizes and replicate.
 # What sample sizes and how many replicates of each.
-nvec <- 3:100
-reps <- 1000
+nvec <- c(3:14,seq(15,100,by=5))
+reps <- 500
 
 # Empty matrices to fill
 pAnova <- matrix(0,nrow=length(nvec),ncol=reps)
@@ -34,8 +34,12 @@ pWilcox[i,r] <- (wilcox.test(d[,1]~d[,2]))$p.value
 
 png('anovaVsWilcox-sampleSize.png')
 par(cex=1.3)
-plot(apply(pAnova,1,mean)~nvec,type='l', col='Blue', xlab='Sample size', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and sd=1')
-lines(apply(pWilcox,1,mean)~nvec,type='l', col='Red', lwd=3)
+plot(apply(pAnova,1,median)~nvec,type='l', col='Blue', xlab='Sample size', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and sd=1')
+lines(apply(pWilcox,1,median)~nvec,type='l', col='Red', lwd=3)
+lines(apply(pAnova, 1, function(x) quantile(x, 0.75))~nvec, lty=2, lwd=2, col='Blue')
+lines(apply(pAnova, 1, function(x) quantile(x, 0.25))~nvec, lty=2, lwd=2, col='Blue')
+lines(apply(pWilcox, 1, function(x) quantile(x, 0.75))~nvec, lty=2, lwd=2, col='Red')
+lines(apply(pWilcox, 1, function(x) quantile(x, 0.25))~nvec, lty=2, lwd=2, col='Red')
 legend('topright', inset=0.01,fill=c('Red','Blue'),legend=c('Anova', 'Wilcox'), box.lwd=2)
 box(lwd=2)
 dev.off()
@@ -72,8 +76,12 @@ pWilcox[i,r] <- (wilcox.test(d[,1]~d[,2]))$p.value
 	
 png('anovaVsWilcox-sd.png')
 par(cex=1.3)
-plot(apply(pAnova,1,mean)~sdvec,type='l', col='Blue', xlab='Standard Deviation', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and n=30')
-lines(apply(pWilcox,1,mean)~sdvec,type='l', col='Red', lwd=3)
+plot(apply(pAnova,1,median)~sdvec,type='l', col='Blue', xlab='Standard Deviation', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and n=30')
+lines(apply(pWilcox,1,median)~sdvec,type='l', col='Red', lwd=3)
+lines(apply(pAnova, 1, function(x) quantile(x, 0.75))~sdvec, lty=2, lwd=2, col='Blue')
+lines(apply(pAnova, 1, function(x) quantile(x, 0.25))~sdvec, lty=2, lwd=2, col='Blue')
+lines(apply(pWilcox, 1, function(x) quantile(x, 0.75))~sdvec, lty=2, lwd=2, col='Red')
+lines(apply(pWilcox, 1, function(x) quantile(x, 0.25))~sdvec, lty=2, lwd=2, col='Red')
 legend('bottomright', inset=0.01,fill=c('Red','Blue'),legend=c('Anova', 'Wilcox'), box.lwd=2)
 box(lwd=2)
 dev.off()
@@ -86,7 +94,7 @@ dev.off()
 # To compare anova and wilcox test, I'll draw numbers from normal distributions and apply the test. Do this for different effect sizes and replicate.
 # What sample sizes and how many replicates of each.
 effectvec <- seq(0.1,5,by=0.1)
-reps <- 50
+reps <- 500
 
 # Empty matrices to fill
 pAnova <- matrix(0,nrow=length(effectvec),ncol=reps)
@@ -108,11 +116,15 @@ pWilcox[i,r] <- (wilcox.test(d[,1]~d[,2]))$p.value
 
 #plot
 	
-png('anovaVsWilcox-sd.png')
+png('anovaVsWilcox-effect.png')
 par(cex=1.3)
-plot(apply(pAnova,1,mean)~sdvec,type='l', col='Blue', xlab='Standard Deviation', ylab='p value', lwd=3, main='rnorms with mean=1 and 1.5 and n=30')
-lines(apply(pWilcox,1,mean)~sdvec,type='l', col='Red', lwd=3)
-legend('bottomright', inset=0.01,fill=c('Red','Blue'),legend=c('Anova', 'Wilcox'), box.lwd=2)
+plot(apply(pAnova,1,median)~effectvec,type='l', col='Blue', xlab='Difference between means', ylab='p value', lwd=3, main='rnorms with sd=2 and n=30')
+lines(apply(pWilcox,1,median)~effectvec,type='l', col='Red', lwd=3)
+lines(apply(pAnova, 1, function(x) quantile(x, 0.75))~effectvec, lty=2, lwd=2, col='Blue')
+lines(apply(pAnova, 1, function(x) quantile(x, 0.25))~effectvec, lty=2, lwd=2, col='Blue')
+lines(apply(pWilcox, 1, function(x) quantile(x, 0.75))~effectvec, lty=2, lwd=2, col='Red')
+lines(apply(pWilcox, 1, function(x) quantile(x, 0.25))~effectvec, lty=2, lwd=2, col='Red')
+legend('topright', inset=0.01,fill=c('Red','Blue'),legend=c('Anova', 'Wilcox'), box.lwd=2)
 box(lwd=2)
 dev.off()
 
