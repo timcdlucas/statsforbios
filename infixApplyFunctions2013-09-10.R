154bf2c704f0fe6c6cb65ba53aa2b54fb1e6ebfd
tim lucas <timcdlucas@gmail.com>
2013-09-10 13:24:41 +0100

    Infix function added

---
+++ infixApplyFunctions2013-09-10.R
@@ -0,0 +1,19 @@
+# infix function for applying a function
+# 10-09-2013
+
+
+# Useful when you want the object to be foremost when reading the 
+
+'%do%' <- function(x, FUN = round){
+ out <- FUN(x)
+}
+
+
+# examples
+c(1:4) %do% mean
+
+x <- c('1','a','3') %do% factor
+
+rnorm(1000) %do% density %do% plot
+
+
