88cc7f5c952cb111b0ad0cd299aec4ca41318df5
tim lucas <timcdlucas@gmail.com>
2014-08-14 15:07:34 +0100

    gbif limits

--- gbifRecords.R
+++ gbifRecords.R
@@ -1,6 +1,17 @@
 
+# limit
+bounds <- c(-125.0, 38.4, -121.8, 40.9)
+occ(query = 'Danaus plexippus', from="inat", limit=250, geometry=bounds) # hangs
+occ(query = 'Danaus plexippus', from="inat", limit=150, geometry=bounds) # works
+
+
 
 x <- occ(query = 'Anopheles plumbeus', geometry = c(-10, 10, 45, 65), from = 'gbif', limit=1000)
 y <- gbif(genus = 'Anopheles', species = 'plumbeus', ext =  c(-10, 10, 45, 65))
 x$gbif$meta$found
 nrow(y)
+
+
+
+
+
