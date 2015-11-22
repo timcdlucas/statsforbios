adfcd492dfba528bba2c9f8362860dd427cc7bf0
Tim Lucas <timcdlucas@gmail.com>
2015-11-22 00:08:33 +0000

    Add adding together even toed ungulate bimass

---
+++ biomass/analysis.R
@@ -0,0 +1,54 @@
+setwd('~/Dropbox/Documents/statsforbios/biomass')
+
+
+library(dplyr)
+library(sqldf)
+
+pantheria <- read.table(file = '~/Dropbox/phd/Documents/thesis/data/Chapter3/PanTHERIA_1-0_WR05_Aug2008.txt',
+  header = TRUE, sep = "\t", na.strings = c("-999", "-999.00"))
+
+d <- read.csv('eventoedbiomass.csv', stringsAsFactors = FALSE)
+
+d$Binomial.name <- gsub('[*]', '', d$Binomial.name)
+
+d <- d[-grep('Domestic', d$Status), ]
+
+
+
+d$pop <- gsub('[*]|[<]|\ ', '', d$Population)
+d$pop <- gsub('\\[.*\\]', '', d$pop)
+
+d$popNum <- NA
+d$popNum[grep('–', d$pop)] <- sapply(strsplit(d$pop[grep('–', d$pop)], '–'), function(x) (as.numeric(x[1]) + as.numeric(x[2]))/2 )
+d$popNum[-grep('–', d$pop)] <- as.numeric(d$pop[-grep('–', d$pop)])
+
+
+d$popNum
+
+mass <- sqldf("
+  SELECT [X5.1_AdultBodyMass_g]
+  AS mass
+  FROM d
+  LEFT JOIN pantheria
+  ON d.[Binomial.name]=pantheria.MSW05_Binomial
+  ")
+
+
+d <- cbind(d, mass)
+
+allMass <- sum(na.omit(d$mass))
+
+#convert to tonnes
+allMasTon <- allMass / 907185
+
+
+
+# 104 species w/ estimates.
+#   Which is about half of ungulates, biased towards more populous species?
+length(na.omit(d$mass))
+
+
+
+
+
+
