74878fd48c7b1bcd054d15b3a5a696c28930c4a3
Tim Lucas <timcdlucas@gmail.com>
2016-05-19 13:19:53 +0100

    ML response curves.

---
+++ differentMLmodels/mlModelResponses.R
@@ -0,0 +1,74 @@
+library(ggplot2)
+library(tidyr)
+library(nnet)
+library(ggthemes)
+library(e1071)
+
+setwd('~/Dropbox/Documents/statsforbios/differentMLmodels')
+
+x = runif(1000, -10, 10)
+y = rnorm(1000, sd = 50) + x^3 
+
+df <- data.frame(x, y)
+ggplot(df, aes(x, y)) + geom_point()
+
+
+newx <- data.frame(x = seq(-15, 15, length.out = 2000))
+
+
+nn4 <- nnet(x, y, size = 4, linout = TRUE)
+nn4.preds <- predict(nn4, newx)
+
+
+nn1 <- nnet(x, y, size = 1, linout = TRUE)
+nn1.preds <- predict(nn1, newx)
+
+
+
+
+nn20 <- nnet(x, y, size = 20, linout = TRUE)
+nn20.preds <- predict(nn20, newx)
+
+
+
+df.out <- cbind(newx, nn4 = nn4.preds, nn1 = nn1.preds, nn20 = nn20.preds)
+
+longdf <- gather(df.out, Model, Prediction, -x)
+
+
+ggplot(longdf, aes(x, Prediction, colour = Model)) + 
+  geom_point(data = df, aes(x, y), colour = 'black', alpha = 0.4) +
+  geom_line(size = 1.3)   +
+  theme_tufte() + theme(text = element_text(size = 30)) + ylab('y')
+
+
+ggsave('neuralNets.png', dpi = 500)
+
+
+
+svmlin <- svm(x, y, kernel = 'linear')
+svmlin.preds <- predict(svmlin, newdata = newx)
+
+svmpoly <- svm(x, y, kernel = 'polynomial')
+svmpoly.preds <- predict(svmpoly, newdata = newx)
+
+
+svmrad <- svm(x, y, kernel = 'radial')
+svmrad.preds <- predict(svmrad, newdata = newx)
+
+
+
+
+df.out <- cbind(newx, linear = svmlin.preds, poly3 = svmpoly.preds, rad = svmrad.preds)
+
+longdf <- gather(df.out, Model, Prediction, -x)
+
+
+ggplot(longdf, aes(x, Prediction, colour = Model)) + 
+  geom_point(data = df, aes(x, y), colour = 'black', alpha = 0.4) +
+  geom_line(size = 1.3)   +
+  theme_dark() + theme(text = element_text(size = 30)) + ylab('y')
+
+ggsave('svm.png', dpi = 500)
+
+
