ff6c83b878e3925ce5cdd7739a56cbe46031c44a
Tim Lucas <timcdlucas@gmail.com>
2016-11-18 10:59:04 +0000

    Start looking at formal exploratory analysis.

---
+++ exploratory/pantheriaexample.R
@@ -0,0 +1,57 @@
+setwd('E://tim/Dropbox/phd/Documents/thesis/')
+
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+library(rpart)
+
+
+pantheria <- read.table(file = 'data/Chapter3/PanTHERIA_1-0_WR05_Aug2008.txt',
+                        header = TRUE, sep = "\t", na.strings = c("-999", "-999.00"))
+
+
+#####################################################
+# Q: What causes Agre at first birth (exploratory)  #
+#####################################################
+
+# Use 90% of data for exploratory analysis
+explore <- sample(1:nrow(pantheria), round(0.9 * nrow(pantheria)))
+nrow(pantheria) - length(explore) # This leaves 500 data points for our hypothesis testing.
+
+panExpl <- pantheria[explore, ]
+
+ggplot(panExpl, aes(x = X3.1_AgeatFirstBirth_d)) + 
+  geom_density() 
+
+ggplot(panExpl, aes(x = X3.1_AgeatFirstBirth_d)) + 
+  geom_density() +
+  scale_x_log10()
+
+# I don't know what causes age at first birth, so chuck a bunch in.
+# Loads of missing data, so do a tree and pick greatest var importance for hypothesis testing.
+Y <- log(panExpl$X3.1_AgeatFirstBirth_d)
+
+# Remove Y, create a bunch of extra feature, add Y back in.
+model <- panExpl %>%
+          select(6:10, 13, 14, 18, 19, 21:33) %>%
+          mutate_all(funs(sqrt, log)) %>% 
+          mutate(X3.1_AgeatFirstBirth_d = Y) %>%
+          rpart(X3.1_AgeatFirstBirth_d ~ ., data = ., control = list(minsplit = 2, cp = 0.001))
+
+model$variable.importance # X23.1_SexualMaturityAge_d is most important... obviously...
+
+#####################################################
+# Q: Does Order cause littersize (test)             #
+#####################################################
+
+# Ignoring phylogeny... Old boss would not be happy. 
+
+panHyp <- pantheria[-explore, ]
+
+hypmodel <- lm(X3.1_AgeatFirstBirth_d ~ X23.1_SexualMaturityAge_d, panHyp)
+anova(hypmodel) # Who woulda thought... it's significant.
+                 
+                 
+                 
+                 
+                 
